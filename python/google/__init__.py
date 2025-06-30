2884cccf25b0d113d63922429565376117e6c9f6
Ge Yunxi <141423244+gyx47@users.noreply.github.com>
2025-06-30 16:57:34 +0800

    drop-pkg-resources-declare
    As of setuptools 81, pkg_resources.declare_namespace has been marked as deprecated (scheduled to be removed after 2025-11-30)
    so I remove it from _init_.py
     
    ```____________ ERROR collecting certbot-dns-google/src/certbot_dns_google/_internal/tests/dns_google_test.py _____________
    src/certbot_dns_google/_internal/tests/dns_google_test.py:9: in <module>
        from google.auth import exceptions as googleauth_exceptions
    /usr/lib/python3.13/site-packages/google/__init__.py:2: in <module>
        __import__('pkg_resources').declare_namespace(__name__)
        ^^^^^^^^^^^^^^^^^^^^^^^^^^^
    /usr/lib/python3.13/site-packages/pkg_resources/__init__.py:98: in <module>
        warnings.warn(
    E   UserWarning: pkg_resources is deprecated as an API. See https://setuptools.pypa.io/en/latest/pkg_resources.html. The pkg_resources package is slated for removal as early as 2025-11-30. Refrain from using this package or pin to Setuptools<81.
    ```

--- python/google/__init__.py
+++ python/google/__init__.py
@@ -1,4 +1,6 @@
-try:
-  __import__('pkg_resources').declare_namespace(__name__)
-except ImportError:
+# try:
+#   __import__('pkg_resources').declare_namespace(__name__)
+# except ImportError:
+
+#drop-pkg-resources-declare
   __path__ = __import__('pkgutil').extend_path(__path__, __name__)
